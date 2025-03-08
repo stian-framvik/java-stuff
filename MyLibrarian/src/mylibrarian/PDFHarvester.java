20a64d84d34d47b3060b71f4442acac294d7b5f1
Stian Framvik <stian.framvik@gmail.com>
2025-03-08 22:24:52 +0100

    PDFHarvester now uses a listAllFilesInDirectory Method that specifies directory or list, and then calls itself recursively if the file is a directory, thus going into subdirectories.

--- MyLibrarian/src/mylibrarian/PDFHarvester.java
+++ MyLibrarian/src/mylibrarian/PDFHarvester.java
@@ -3,19 +3,33 @@ package MyLibrarian.src.mylibrarian;
 import java.io.File;
 
 public class PDFHarvester {
+
+    // The main function goes on top, probably?
     public static void main(String[] args) {
-        // String directoryString = "E:\Zotero\storage";
 
-        File[] files =  new File("E:\\Zotero\\storage").listFiles();
-        
+        // TODO: Prompt user for 'directoryPath' & create File[] = new File(directoryPath).
 
-        for (File file : files) {
-            if (file.isDirectory()) {
-                System.out.println("Directory: " + file.getName());
+        // Run method to list all files in the given directory.
+        listAllFilesInDirectory(new File("E:\\Zotero\\storage\\"));
+    }
+
+    static void listAllFilesInDirectory(File directory) {
+        // Create File array from directory.
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.println("Directory: " + file.getName());
+                    
+                    listAllFilesInDirectory(file);
+                }
+                else {
+                    System.out.println("File: " + file.getName());
+                }
             }
-            else {
-                System.out.println("File " + file.getName());
-            }
+        } else { 
+            System.out.println("The directory is empty or does not exist.");
         }
     }
 }
