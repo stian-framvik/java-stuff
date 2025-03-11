e7901ee61bc03035c5d8668e4226cbe707f57e41
Stian Framvik <stian.framvik@gmail.com>
2025-03-11 12:42:54 +0100

    Implemented method to check file extension, now correctly IDs PDFs.

--- MyLibrarian/src/mylibrarian/PDFHarvester.java
+++ MyLibrarian/src/mylibrarian/PDFHarvester.java
@@ -13,23 +13,49 @@ public class PDFHarvester {
         listAllFilesInDirectory(new File("E:\\Zotero\\storage\\"));
     }
 
-    static void listAllFilesInDirectory(File directory) {
+    public static void listAllFilesInDirectory(File directory) {
         // Create File array from directory.
         File[] files = directory.listFiles();
 
+        // If-statement to catch empty arrays. If 'null', then print error.
         if (files != null) {
+
+            // Loop through all files in the 'files' array.
             for (File file : files) {
                 if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    
                     listAllFilesInDirectory(file);
                 }
                 else {
-                    System.out.println("File: " + file.getName());
+                    String fileExtension = getFileExtension(file);
+
+                    if ("pdf".equals(fileExtension)) {
+                        System.out.println("File: " + file.getName());
+                    }
                 }
             }
-        } else { 
+            } else { 
             System.out.println("The directory is empty or does not exist.");
+            }
         }
-    }
+
+    // Method for extracting file extensions.
+    public static String getFileExtension(File file) {
+        String name = file.getName();
+        int lastIndexOf = name.lastIndexOf(".");
+        if (lastIndexOf == -1) {
+            return "";
+        }
+        else {
+            return name.substring(lastIndexOf + 1);
+        }
+   }
 }
+
+// else {
+//     // System.out.println("File: " + file.getName());
+//     String fileExtension = getFileExtension(file);
+//     if (fileExtension == ".pdf") {
+//         System.out.println("File extension: " + fileExtension);
+//     }
+// }
+// }
